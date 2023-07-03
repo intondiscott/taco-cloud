fb84a8f81326cfae557325b842394d1f1a48c6fc
intondiscott@gmail.com <you@example.com>
2023-07-03 15:56:30 -0400

    uploaded files

--- taco-cloud/src/main/java/tacos/TacoCloudApplication.java
+++ taco-cloud/src/main/java/tacos/TacoCloudApplication.java
@@ -2,6 +2,8 @@ package tacos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class TacoCloudApplication {
@@ -10,4 +12,12 @@ public class TacoCloudApplication {
 		SpringApplication.run(TacoCloudApplication.class, args);
 	}
 
+}
+
+@Controller
+class HomeController {
+	@GetMapping("/")
+	String home(){
+		return "index";
+	}
 }
