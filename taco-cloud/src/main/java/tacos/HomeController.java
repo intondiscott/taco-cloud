9bc3667439dad262ac390481995493a704f618ae
intondiscott@gmail.com <you@example.com>
2023-07-04 11:16:02 -0400

    uploaded files

--- taco-cloud/src/main/java/tacos/HomeController.java
+++ taco-cloud/src/main/java/tacos/HomeController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-class HomeController {
+public class HomeController {
     @GetMapping("/")
     String home(){
         return "index";
--- taco-cloud/src/main/java/tacos/Taco.java
+++ taco-cloud/src/main/java/tacos/Taco.java
@@ -1,8 +1,16 @@
 package tacos;
 
-import java.util.List;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
 
+import java.util.List;
+@Data
 public class Taco {
+    @NotNull
+    @Size(min=5,message = "Most be at least 5 characters long")
     private String name;
+    @NotNull
+    @Size(min=1, message = "Must choose at least 1 ingredient")
     private List<Ingredient> ingredients;
 }
